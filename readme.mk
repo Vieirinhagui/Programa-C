a38ee3ef93111d183f21ac00755b1dd23f9e4c2c
Eduardo Antero de Moraes <96095997+duduantero@users.noreply.github.com>
2022-10-25 13:34:59 -0300

    att

--- readme.mk
+++ readme.mk
@@ -2,7 +2,7 @@
 
 > Sistema desenvolvido para matéria de Engenharia de Requisitos do curso de Engenharia de Software da Universidade Católica de Brasília.
 
-![prewview]()
+![prewview](https://cdn.discordapp.com/attachments/982291225283551262/1034505021154398218/unknown.png)
 
 ## 🛠 Tecnologias
 
@@ -13,11 +13,11 @@
 ## Instalando e configurando
 
 -Instalando o MySQL Connector/C
-1- Realizar o download do arquivo ZIP em MySQL Product Archives <https://downloads.mysql.com/archives/c-c/> de acordo com seu sistema operacional.
+1- Realizar o download do arquivo ZIP em [MySQL Product Archives] (https://downloads.mysql.com/archives/c-c/) de acordo com seu sistema operacional.
 2- Criar uma pasta com nome "ConectorC"no Disco Local C , após isso deve ser extraido o arquivo baixado nesta pasta.
 
 -Instalando Codeblocks
-1- Realizar o dowloado do instalador em [https://www.codeblocks.org/downloads/binaries/](https://www.codeblocks.org/downloads/binaries/).
+1- Realizar o dowloado do instalador em [Code::Blocks](https://www.codeblocks.org/downloads/binaries/).
 2- Realizar instalação padrão do software.
 
 -Configurando o MySQL Connector/C no Codeblocks
