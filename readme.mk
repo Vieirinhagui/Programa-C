9bc3e81e3fa95d4bc2ce9f2ca933e8cb3a692fad
Eduardo Antero de Moraes <96095997+duduantero@users.noreply.github.com>
2022-10-25 12:53:08 -0300

    Update readme

--- readme.mk
+++ readme.mk
@@ -1,1 +1,13 @@
+# ePET
 
+> Sistema desenvolvido para matéria de Engenharia de Requisitos do curso de Engenharia de Software da Universidade Católia de Brasília.
+
+![prewview]()
+
+## 🛠 Tecnologias
+
+C
+SQL
+XAMPP
+
+##
