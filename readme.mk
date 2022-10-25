52a07e64e473499cfd94691e7e7ccf4e8424172f
Eduardo Antero de Moraes <96095997+duduantero@users.noreply.github.com>
2022-10-25 13:23:40 -0300

    att

--- readme.mk
+++ readme.mk
@@ -8,7 +8,26 @@
 
   -C
   -MYSQL
-  -[CONNECTORC - MYSQL}(https://downloads.mysql.com/archives/c-c/)
+  -CONNECTOR/C - MYSQL
   -XAMPP
+## Instalando e configurando
 
-##
+-Instalando o MySQL Connector/C
+1- Realizar o download do arquivo ZIP em [https://downloads.mysql.com/archives/c-c/](https://downloads.mysql.com/archives/c-c/) de acordo com seu sistema operacional.
+2- Criar uma pasta com nome "ConectorC"no Disco Local C , após isso deve ser extraido o arquivo baixado nesta pasta.
+
+-Instalando Codeblocks
+1- Realizar o dowloado do instalador em [https://www.codeblocks.org/downloads/binaries/](https://www.codeblocks.org/downloads/binaries/).
+2- Realizar instalação padrão do software.
+
+-Configurando o MySQL Connector/C no Codeblocks
+Nesta etapa será feita a inserção da bliblioteca na IDE.
+
+Settings>Compiler>Linker Compiler
+Settings>Compiler>Search Compiler
+
+1-
+2-
+
+
+
